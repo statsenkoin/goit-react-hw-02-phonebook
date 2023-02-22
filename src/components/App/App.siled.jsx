1d5540a84836c3f5c9e9048d3625e59f9cce42d8
Ihor Statsenko <statsenkoin@gmail.com>
2023-02-22 14:12:22 +0200

    Update App.siled.jsx

--- src/components/App/App.siled.jsx
+++ src/components/App/App.siled.jsx
@@ -3,8 +3,9 @@ import styled from 'styled-components';
 export const Layout = styled.div`
   height: 100vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  font-size: 40px;
+  font-size: 28px;
   color: #010101;
 `;
