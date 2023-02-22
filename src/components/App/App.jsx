2cff6f223bf41bebb7c67ded233c9d7e49c0b4e6
Ihor Statsenko <statsenkoin@gmail.com>
2023-02-22 14:12:55 +0200

    Feature/ add base form markup

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,10 +2,55 @@ import React, { Component } from 'react';
 import { Layout } from './App.siled';
 
 class App extends Component {
-  state = { a: '3' };
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+  };
+
+  handleInputChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    console.log('this.state :>> ', this.state);
+  };
 
   render() {
-    return <Layout>Phonebook</Layout>;
+    return (
+      <Layout>
+        <h1>Phonebook</h1>
+        <form onSubmit={this.onSubmit}>
+          <label htmlFor="name">
+            Name
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleInputChange}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <label htmlFor="tel">
+            Number
+            <input
+              type="tel"
+              name="number"
+              value={this.state.number}
+              onChange={this.handleInputChange}
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+      </Layout>
+    );
   }
 }
 
