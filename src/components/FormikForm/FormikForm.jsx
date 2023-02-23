be427d289842ac72895fce198b1004c519a67e57
Ihor Statsenko <statsenkoin@gmail.com>
2023-02-23 23:02:05 +0200

    Feature/ Add validetion

--- src/components/FormikForm/FormikForm.jsx
+++ src/components/FormikForm/FormikForm.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Formik, Field, Form } from 'formik';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
 import { nanoid } from 'nanoid';
+import * as Yup from 'yup';
+
+const SubmitSchema = Yup.object().shape({
+  name: Yup.string().required('Required'),
+  number: Yup.string().required('Required'),
+});
 
 export const FormikForm = ({ onSubmit }) => {
   return (
@@ -12,6 +18,7 @@ export const FormikForm = ({ onSubmit }) => {
           name: '',
           number: '',
         }}
+        validationSchema={SubmitSchema}
         onSubmit={(values, { resetForm }) => {
           onSubmit({
             ...values,
@@ -24,10 +31,12 @@ export const FormikForm = ({ onSubmit }) => {
           <label htmlFor="name">
             Name
             <Field type="text" name="name" />
+            <ErrorMessage name="name"></ErrorMessage>
           </label>
           <label htmlFor="number">
             Number
             <Field type="text" name="number" />
+            <ErrorMessage name="number"></ErrorMessage>
           </label>
           <button type="submit">Add contact</button>
         </Form>
